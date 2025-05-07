68291809e3d87f94605edeb6580a30158a5c5762
romanchykov-webdev <romanchykov.webdev@gmail.com>
2025-05-07 18:50:22 +0200

    add Pagination and change api endpoint

---
+++ mokData/search.js
@@ -0,0 +1,122 @@
+export const search = [
+	{
+		id: '107835af-9698-4edc-b7d3-529200c4b28e',
+		title: "Cavs coach Kenny Atkinson calls out Pacers, suggests team crossed 'line of physicality' in playoff game",
+		description:
+			"The Cleveland Cavaliers entered this year's playoffs as the top seed in the Eastern Conference then cruised through a first-round series with the Miami Heat.After sweeping the Heat, the Cavs opened a second-round series with the Indiana Pacers Sunday. The Pacers upset the Cavs in Game 1 of the East ...",
+		url: 'https://www.foxnews.com/sports/cavs-coach-kenny-atkinson-calls-out-pacers-suggests-team-crossed-line-physicality-playoff-game',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2025/05/kenny-atkinson.jpg',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 02:01:28 +0000'
+	},
+	{
+		id: '3a5d4b64-ac97-47e4-9fa7-17240148b57f',
+		title: 'Yankees legend Derek Jeter makes crucial life message hilarious in speech to Michigan graduates',
+		description:
+			"Derek Jeter delivered the commencement speech at the University of Michigan Saturday and spoke about the importance of fighting through failure. The New York Yankees legend would've played baseball at Michigan, the state he called home in Kalamazoo, if he didn't turn pro right away as a first-round ...",
+		url: 'https://www.foxnews.com/sports/yankees-legend-derek-jeter-makes-crucial-life-message-hilarious-speech-michigan-graduates',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2022/09/Derek-Jeter2.jpg',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 01:42:50 +0000'
+	},
+	{
+		id: 'ea53ce81-82d9-42a6-b67b-64aceb9c7ecb',
+		title: 'Noem calls for the death penalty following maritime human smuggling attempt that left child dead',
+		description:
+			"Homeland Security Secretary Kristin Noem called for the death penalty in the wake of a disastrous maritime human smuggling operation that left one child, and at least two others, dead.The U.S. Attorney's Office for the Southern District of California announced charges Tuesday against five illegals i...",
+		url: 'https://www.foxnews.com/politics/five-illegals-charged-alleged-maritime-human-smuggling-attempt-left-child-dead',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2025/05/border-patrol-san-diego.png',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 01:29:12 +0000'
+	},
+	{
+		id: '4649148a-752d-4611-93c7-fd7d0f518e67',
+		title: 'Pirates security guard whips fan with belt during stadium fight and gets suspended',
+		description:
+			'A Pittsburgh Pirates stadium usher has been suspended after being seen using his belt to whip a fan during a fistfight at PNC Park on Sunday.The video begins by showing the usher and fan standing face-to-face in a heated argument before the staffer punches the fan in the face multiple times. CLICK H...',
+		url: 'https://www.foxnews.com/sports/pirates-security-guard-whips-fan-belt-during-stadium-fight-gets-suspended',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2023/06/Pirates-hat.jpg',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 01:04:15 +0000'
+	},
+	{
+		id: '935b039e-d09d-4169-a9f6-3830bdbe54b3',
+		title: "'The View' co-host insists Gloria Gaynor is actually a feminist after singer rejects title",
+		description:
+			'The ladies of "The View" tried to make sense of why Gloria Gaynor does not consider her iconic song, "I Will Survive," to be a feminist anthem and why she would not label herself a feminist.In a recent interview with the UK outlet Metro, Gaynor said that one of the biggest misconceptions about her i...',
+		url: 'https://www.foxnews.com/media/the-view-co-host-insists-gloria-gaynor-actually-feminist-after-singer-rejects-title',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2025/05/gloria-gaynor.jpg',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 01:00:35 +0000'
+	},
+	{
+		id: 'bd97fbb9-4f6c-4914-8cc7-965a038baf78',
+		title: "Astounding new audio shows air traffic controllers reacting to FAA tech outage: 'This is a serious issue'",
+		description:
+			'Incredible air traffic control (ATC) audio captures the moment staff lost radar and radio signals while guiding planes to Newark Liberty International Airport in New Jersey last week.The April 28 outage lasted roughly 90 seconds. The National Air Traffic Controllers Association confirmed the inciden...',
+		url: 'https://www.foxnews.com/travel/astounding-new-audio-shows-air-traffic-controllers-reacting-faa-tech-outage-this-serious-issue',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2025/05/newark-tower.jpg',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 00:53:28 +0000'
+	},
+	{
+		id: '232d630a-f579-4729-8a74-96137c3148da',
+		title: "Scott Bessent says US doesn't want to decouple from China ahead of scheduled meeting with Chinese counterpart",
+		description:
+			'Treasury Secretary Scott Bessent and U.S. Trade Representative Jamieson Greer will meet with their Chinese counterparts over the weekend in Switzerland to discuss economic matters, a significant first step in trade talks between the two global superpowers since President Donald Trump announced sweep...',
+		url: 'https://www.foxnews.com/media/scott-bessent-says-us-doesnt-want-decouple-from-china-ahead-scheduled-meeting-chinese-counterpart',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2025/05/treasury-sec-scott-bessent.png',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 00:53:08 +0000'
+	},
+	{
+		id: 'e6c0337d-68d5-4467-8396-ed47694e3640',
+		title: 'Trump says less than 2 dozen hostages believed to be alive in Gaza',
+		description:
+			"What we know about Israel's plans to capture the entire Gaza Strip\n\nWhat we know about Israel's plans to capture Gaza\n\nWhat we know about Israel's plans to capture Gaza\n\nPresident Trump said Tuesday t...",
+		url: 'https://www.cbsnews.com/news/trump-says-less-than-2-dozen-hostages-believed-to-be-alive-in-gaza/',
+		author: 'cbsnews',
+		image: 'https://assets2.cbsnewsstatic.com/hub/i/r/2025/05/07/0f9f983c-8c4b-4cca-b7a1-4f51038547a3/thumbnail/1200x630/12c8cf6ba5b5384629818cd2f3352bc1/gettyimages-2213213417.jpg?v=8782d831dd587e69db5d043c0a4c5f4b',
+		language: 'en',
+		category: ['politics', 'world'],
+		published: '2025-05-07 00:42:28 +0000'
+	},
+	{
+		id: 'ddc40d9d-770c-47f3-8a26-a2c0169bcb60',
+		title: 'Ex-NFL player speaks out against college athlete revenue sharing with landmark Supreme Court ruling pending',
+		description:
+			'The age of athlete compensation in college sports could get a lot more complicated with a pending Supreme Court decision. A settlement in the case of House v. NCAA would allow all Division I schools to share revenue directly with student athletes if it receives final approval this month.The current ...',
+		url: 'https://www.foxnews.com/sports/ex-nfl-player-speaks-out-against-college-athlete-revenue-sharing-landmark-supreme-court-ruling-pending',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2025/02/supreme-court.jpg',
+		language: 'en',
+		category: ['general'],
+		published: '2025-05-07 00:37:54 +0000'
+	},
+	{
+		id: '8f2d7933-0c0b-4b97-a36f-68b5aeb1bca2',
+		title: "DeSantis signs bill banning fluoride additives in Florida public water: 'Hydrate, not medicate'",
+		description:
+			'Florida Gov. Ron DeSantis signed SB 700 into law Tuesday, eliminating the ability of local governments to add fluoride or other medical additives to the state\'s public water supply, a move cheered by supporters of medical freedom and health transparency."Jamming fluoride in the water supply …  is es...',
+		url: 'https://www.foxnews.com/health/desantis-signs-bill-banning-fluoride-additives-florida-public-water-hydrate-not-medicate',
+		author: 'foxnews',
+		image: 'https://static.foxnews.com/foxnews.com/content/uploads/2025/05/desantis-fluoride.jpg',
+		language: 'en',
+		category: ['health', 'lifestyle'],
+		published: '2025-05-07 00:20:06 +0000'
+	}
+]
